1c3911db03a8797cd7fea843aa2b799ff2188f6b
snipzer <l.arif@laposte.net>
2017-06-22 17:02:07 +0200

    #24 Création des formulaires pour Job et PersonnalityType ainsi que leurs vérifications

---
+++ src/AdminBundle/Form/PersonnalityTypeType.php
@@ -0,0 +1,30 @@
+<?php
+namespace AdminBundle\Form;
+
+use AdminBundle\Entity\PersonnalityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonnalityTypeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("name", TextType::class, ["label" => "Nom du type"])
+            ->add("personnalityType", TextType::class, ["label" => "Type de personnalité"])
+            ->add("opposedPersonnalityType", TextType::class, ["label" => "Type de personnalité opposés"])
+            ->add("save", SubmitType::class, ["label" => "Créer un type de personnalité"]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            "data_class" => PersonnalityType::class,
+            "csrf_protection" => true,
+            "csrf_field_name" => '_token',
+            "csrf_token_id" => "personnalityType_item",
+        ]);
+    }
+}
