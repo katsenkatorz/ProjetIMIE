8849be0b5d45507079f9cf710eab1134b183e0ef
snipzer <l.arif@laposte.net>
2017-06-23 14:30:32 +0200

    #30 Ajout de deux méthodes       -getJobPersonnalityByPtIdAndJobId() qui renvois un JobPersonnality choisie par sont job et sont personnality type       -putJobPersonnalityByPtidAndJobId() qui modifie un JobPersonnality selectionner grâce à sont job et personnalityType

--- src/AdminBundle/Repository/JobPersonnalityRepository.php
+++ src/AdminBundle/Repository/JobPersonnalityRepository.php
@@ -36,6 +36,60 @@ class JobPersonnalityRepository extends \Doctrine\ORM\EntityRepository
     }
 
     /**
+     * Renvois un JobPersonnality choisie par sont job et sont personnality type
+     *
+     * @param $jobId
+     * @param $ptId
+     * @return null|object
+     */
+    public function getJobPersonnalityByPtIdAndJobId($jobId, $ptId)
+    {
+        $job = $this->getEntityManager()->getRepository("AdminBundle:Job")->getJobById($jobId);
+        $pt = $this->getEntityManager()->getRepository("AdminBundle:PersonnalityType")->getPersonnalityTypeById($ptId);
+
+        return $this->findOneBy(["job" => $job, "personnalityType" => $pt]);
+    }
+
+    /**
+     * Renvois les types de personnalités lié a un job
+     *
+     * @param $jobId
+     * @return bool|array
+     */
+    public function getPersonnalityTypesByJobId($jobId)
+    {
+        $job = $this->getEntityManager()->getRepository("AdminBundle:Job")->getJobById($jobId);
+
+        $JobPersonnalities = $this->findBy(['job' => $job]);
+
+
+        $result = [];
+        if(count($JobPersonnalities) > 0)
+        {
+            foreach($JobPersonnalities as $jobPersonnality)
+            {
+                $value = $jobPersonnality->getValue();
+                $name = $jobPersonnality->getPersonnalityType()->getName();
+                $personnalityType = $jobPersonnality->getPersonnalityType()->getPersonnalityType();
+                $opposedPersonnalityType = $jobPersonnality->getPersonnalityType()->getOpposedPersonnalityType();
+                $idJP = $jobPersonnality->getId();
+
+                $result[$name] = [
+                    "name" => $name,
+                    "value" => $value,
+                    "idJP" => $idJP,
+                    "personnalityType" => $personnalityType,
+                    "opposedPersonnalityType" => $opposedPersonnalityType
+                ];
+            }
+
+            return $result;
+        }
+
+        return false;
+    }
+
+    /**
      * Créer un nouveau jobPersonnality
      *
      * @param $value
@@ -65,25 +119,58 @@ class JobPersonnalityRepository extends \Doctrine\ORM\EntityRepository
     }
 
     /**
-     * Modifie un JobPersonnality
+     * Modifie un JobPersonnality avec une id de jobPersonnality
      *
-     * @param $id
+     * @param $idJP
      * @param $value
      * @param Job $job
      * @param PersonnalityType $personnalityType
      *
      * @return bool|object
      */
-    public function putJobPersonnality($id, $value, Job $job, PersonnalityType $personnalityType)
+    public function putJobPersonnalityByJpid($idJP, $value, Job $job, PersonnalityType $personnalityType)
     {
         $em = $this->getEntityManager();
 
-        $jobPersonnality = $this->getJobPersonnalityById($id);
+        $jobPersonnality = $this->getJobPersonnalityById($idJP);
 
         if(!is_null($jobPersonnality) && (!is_null($value) && is_numeric($value)))
         {
             $jobPersonnality->setValue($value)
                 ->setPersonnalityType($personnalityType)
+                ->setJob($job);
+
+            $em->persist($jobPersonnality);
+            $em->flush();
+
+            return $jobPersonnality;
+        }
+
+        return false;
+    }
+
+    /**
+     * Modifie un JobPersonnality selectionner grâce à sont job et personnalityType
+     *
+     * @param $value
+     * @param $jobId
+     * @param $ptId
+     *
+     * @return bool|null|object
+     */
+    public function putJobPersonnalityByPtidAndJobId($value, $jobId, $ptId)
+    {
+        $em = $this->getEntityManager();
+        $job = $this->getEntityManager()->getRepository("AdminBundle:Job")->getJobById($jobId);
+        $pt = $this->getEntityManager()->getRepository("AdminBundle:PersonnalityType")->getPersonnalityTypeById($ptId);
+
+
+        $jobPersonnality = $this->getJobPersonnalityByPtIdAndJobId($jobId, $ptId);
+
+        if(!is_null($jobPersonnality) && (!is_null($value) && is_numeric($value)))
+        {
+            $jobPersonnality->setValue($value)
+                ->setPersonnalityType($pt)
                 ->setJob($job);
 
             $em->persist($jobPersonnality);
