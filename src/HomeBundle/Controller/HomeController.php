2ed4cee21d528efb9a20afb5f26cc10041b86cf7
CMaxime <cabon.maxime@gmail.com>
2017-07-12 09:52:15 +0200

    #69 Creation des vues dans HomeBundle

--- src/HomeBundle/Controller/HomeController.php
+++ src/HomeBundle/Controller/HomeController.php
@@ -13,21 +13,21 @@ class HomeController extends Controller
 
         $jobs = $JobRepository->getJobs();
 
-        $jobs = array_slice($jobs, 0, 3 );
+        $jobs = array_slice($jobs, 0, 3);
 
         return $this->render('HomeBundle:app:home.html.twig', [
             "jobs" => $jobs,
         ]);
     }
 
-    public function jobsAction()
+    public function metiersAction()
     {
         // Récupération des répository et manager
         $JobRepository = $this->getDoctrine()->getRepository("AdminBundle:Job");
 
         $jobs = $JobRepository->getJobs();
 
-        return $this->render('HomeBundle:app:jobs.html.twig', [
+        return $this->render('HomeBundle:app:metiers.html.twig', [
             "jobs" => $jobs,
         ]);
     }
@@ -42,8 +42,36 @@ class HomeController extends Controller
         return $this->render('HomeBundle:app:question2.html.twig');
     }
 
-    public function resultAction()
+    public function metierAction()
     {
-        return $this->render('HomeBundle:app:result.html.twig');
+        return $this->render('HomeBundle:app:metier.html.twig');
+    }
+
+    public function renderFooterAction()
+    {
+        $ParamRepo = $this->getDoctrine()->getRepository("AdminBundle:Parameters");
+
+        $parameters = $ParamRepo->getParameters();
+
+//        $mentionLegale = $ParamRepo->getParameterById(4);
+//        $proprietaire = $ParamRepo->getParameterById(1);
+//        $address = $ParamRepo->getParameterById(2);
+//        $copyright = $ParamRepo->getParameterById(3);
+
+        return $this->render('HomeBundle:layout:footer.html.twig', [
+            'parameters' => $parameters,
+        ]);
+    }
+
+    public function mentionsLegalesAction()
+    {
+        $ParamRepo = $this->getDoctrine()->getRepository("AdminBundle:Parameters");
+
+        $mentionsLegales = $ParamRepo->getParameterById(4);
+
+
+        return $this->render('HomeBundle:app:mentionsLegales.html.twig', [
+            'mentionsLegales' => $mentionsLegales,
+        ]);
     }
 }
