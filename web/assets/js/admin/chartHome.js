72c55d81baf449bcfd71dda96229816c7a93fec4
CMaxime <cabon.maxime@gmail.com>
2017-07-31 14:43:16 +0200

    #89 ajout du chart dans home

--- web/assets/js/admin/chartHome.js
+++ web/assets/js/admin/chartHome.js
@@ -1,6 +1,34 @@
-var ctx = document.getElementById("pieChart").getContext('2d');
-var pieChart = new Chart(ctx,{
-    type: 'pie',
-    data: data,
-    options: options
+$(document).ready(function () {
+    var inputs = document.querySelectorAll(".dataHolder");
+
+    var labels = [];
+    var values = [];
+
+    inputs.forEach(function (elem) {
+        labels.push(elem.dataset.type);
+
+        if (elem.value < 0)
+            elem.value = -elem.value;
+
+        values.push(parseInt(elem.value));
+    });
+
+    var ctx = document.getElementById("pieChart").getContext('2d');
+    var pieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ["Esprit", "Energie", "Nature", "Tactique", "Identité"],
+            datasets: [{
+                label: "Population (millions)",
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                data: [2478, 5267, 734, 784, 433]
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Nombre de question ajoutées'
+            }
+        }
+    });
 });
