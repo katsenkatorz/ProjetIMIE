7b529c0ddac697f4a1bf6f93dd139fef8583239f
snipzer <l.arif@laposte.net>
2017-09-05 09:52:40 +0200

    #93 Mise en place du recaptcha google 3

--- web/assets/js/home/QuizzProcess.js
+++ web/assets/js/home/QuizzProcess.js
@@ -1,4 +1,16 @@
 var recaptcha;
+
+function onloadCallback()
+{
+    return recaptcha = grecaptcha.render('g-recaptcha', {
+        sitekey: "6Ldsay8UAAAAAGUTeta9WaLv-t4ts1WnqA7Pk_-O",
+        size:"invisible",
+        callback: function (paramOne) {
+            console.log(paramOne);
+        }
+    })
+}
+
 $(document).ready(function ()
 {
 
@@ -541,19 +553,6 @@ function getQuestionSetIntoLocalStorage(callback)
 
     // On fait les traitements à la fin de la requête ajax
     callback();
-}
-
-function onloadCallback()
-{
-    return recaptcha = grecaptcha.render('g-recaptcha', {
-        sitekey: "6Ldsay8UAAAAAGUTeta9WaLv-t4ts1WnqA7Pk_-O",
-        size:"invisible",
-        callback: function (paramOne, paramTwo, paramTree) {
-            console.log(paramOne);
-            console.log(paramTwo);
-            console.log(paramTree);
-        }
-    })
 }
 
 
